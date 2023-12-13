8796d6b0c646fa0a0e61c60466fa0626ccd58969
GZsean <657641986@qq.com>
2023-12-14 01:05:56 +0800

    add home page

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,15 +1,24 @@
 import Link from "next/link";
 
 const Navbar = () => {
+  const pageConfig = [
+    {
+      link: "/",
+      title: "Home",
+    },
+    {
+      link: "/List",
+      title: "Articles",
+    },
+  ];
   return (
     <nav className="bg-[#6d8877] text-white p-4 fixed w-screen z-10">
       <ul className="flex space-x-4">
-        <li className="px-2">
-          <Link href="/">Home</Link>
-        </li>
-        <li>
-          <Link href="/List">Articles</Link>
-        </li>
+        {pageConfig.map((item) => (
+          <li className="px-2">
+            <Link href={item.link}>{item.title}</Link>
+          </li>
+        ))}
       </ul>
     </nav>
   );
